678b0c090c02a4acb0a53b67af83610e9f07f901
Rémi K. <remi.kiragossian@gmail.com>
2025-01-27 11:15:47 +0100

    Update entity.py
    Generate unique id for all entites. 
    
    Fix attempts for this issue : https://github.com/antibill51/rika-firenet-custom-component/issues/19
    
    Please note, you must delete and recreate the device to avoid duplicate entities.

--- custom_components/rika_firenet/entity.py
+++ custom_components/rika_firenet/entity.py
@@ -8,32 +8,44 @@ _LOGGER = logging.getLogger(__name__)
 
 
 class RikaFirenetEntity(CoordinatorEntity):
+    """Base class for all Rika Firenet entities."""
+
     def __init__(self, config_entry, stove: RikaFirenetStove, coordinator: RikaFirenetCoordinator, suffix=None):
+        """Initialize the entity."""
         super().__init__(coordinator)
+
         self._config_entry = config_entry
         self._stove = stove
-        self._u_id = stove.get_id()
-        if suffix is not None:
-            self._name = f"{stove.get_name()} {suffix}"
-            self._unique_id = f"{suffix} {stove.get_name()}"
-        else:
-            self._name = stove.get_name()
-            self._unique_id = stove.get_id()
-        _LOGGER.debug('RikaFirenetEntity creation with name: ' + self._name + ' unique_id: ' + self._unique_id)
+        self._stove_id = stove.get_id()
+        self._suffix = suffix
+        self._name = f"{stove.get_name()} {suffix}" if suffix else stove.get_name()
+        self._unique_id = self._generate_unique_id()
+
+        _LOGGER.debug("Created RikaFirenetEntity: name=%s, unique_id=%s", self._name, self._unique_id)
+
+    def _generate_unique_id(self):
+        """Generate a unique ID for the entity."""
+        if self._suffix:
+            # Combine stove ID and suffix for uniqueness
+            return f"{self._stove_id}_{self._suffix}".replace(" ", "_").lower()
+        return self._stove_id.replace(" ", "_").lower()
 
     @property
     def unique_id(self):
+        """Return the unique ID of the entity."""
         return self._unique_id
 
     @property
     def name(self):
+        """Return the name of the entity."""
         return self._name
 
     @property
     def device_info(self):
+        """Return the device information."""
         return {
-            "identifiers": {(DOMAIN, self._u_id)},
-            "name": NAME,
+            "identifiers": {(DOMAIN, self._stove_id)},
+            "name": self._stove.get_name(),
             "model": VERSION,
             "manufacturer": DEFAULT_NAME,
         }
