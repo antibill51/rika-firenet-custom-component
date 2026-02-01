dc0f26ad02e8c27ff036c6b582af6af570e9fd64
Rémi K. <remi.kiragossian@gmail.com>
2026-02-01 08:16:44 +0100

    new version

--- custom_components/rika_firenet/const.py
+++ custom_components/rika_firenet/const.py
@@ -28,7 +28,7 @@ SUMMARY_URL = f"{BASE_URL}/web/summary"
 STATUS_URL = f"{BASE_URL}/api/client/{{stove_id}}/status"
 CONTROLS_URL = f"{BASE_URL}/api/client/{{stove_id}}/controls"
 
-VERSION = "2.29.37"
+VERSION = "2.29.38"
 DOMAIN = "rika_firenet"
 
 UNIQUE_ID = "unique_id"
